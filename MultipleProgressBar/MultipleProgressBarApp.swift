35ba3b9001e31fb5cf80849d70ab92977d48bf5d
RajendranEshwaran <rajendranmca2010@gmail.com>
2022-03-20 19:03:58 -0400

    Initial Commit

---
+++ MultipleProgressBar/MultipleProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleProgressBarApp.swift
+//  MultipleProgressBar
+//
+//  Created by gomathi saminathan on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MultipleProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
